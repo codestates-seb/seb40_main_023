746f19eaa7cf52d5742e9f996e4970ea9783137e
Dev-Sam32 <90596545+Dev-Sam32@users.noreply.github.com>
2022-11-14 11:24:00 +0900

    Revert "Revert "[FEAT]API Member_CRUD기능구현""

--- server/src/main/java/seb40/main023/server/member/dto/MemberResponseDto.java
+++ server/src/main/java/seb40/main023/server/member/dto/MemberResponseDto.java
@@ -1,5 +1,24 @@
 package seb40.main023.server.member.dto;
 
-public class MemberResponseDto {
+import lombok.Builder;
+import lombok.Getter;
+import seb40.main023.server.luckMango.entity.LuckMango;
+import seb40.main023.server.review.entity.Review;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+public class MemberResponseDto {
+    private long memberId;
+    private String name;
+    private String email;
+    private String password;
+    private int nyMoney;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    private List<LuckMango> luckMangos;
+    private List<Review> reviews;
 }

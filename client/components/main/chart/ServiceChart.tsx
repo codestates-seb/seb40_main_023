482570ff1c82a3c79b88bf7f14ffb479a3e1a477
MA <36831218+rmaomina@users.noreply.github.com>
2022-11-18 01:15:04 +0900

    feat/메인 차트 섹션 애니메이션 구현 (#52)
    * feat/메인 카운트업 스크롤 트리거에 애니메이션 기능 구현

--- client/components/main/chart/ServiceChart.tsx
+++ client/components/main/chart/ServiceChart.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { useInView, InView } from "react-intersection-observer";
+import CountUp from "react-countup";
 import LineChart from "./Chart";
 
 const getCurrentTime = (): string => {
@@ -11,22 +13,52 @@ const getCurrentTime = (): string => {
 
 const ServiceChart = () => {
   const [currentTime, setCurrentTime] = useState("");
+  const [countUp, setCountUp] = useState(false);
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: 0,
+  });
 
   useEffect(() => {
     setCurrentTime(getCurrentTime());
   }, []);
 
+  useEffect(() => {
+    setCountUp(inView);
+    console.log(entry);
+  }, [inView]);
+
   return (
-    <div>
+    <div ref={ref}>
       <div className="grid w-full grid-cols-1 text-center mobile:text-left mobile:grid-cols-2">
         <div className="flex flex-col p-4 pt-8 mobile:pl-[6rem]">
           <dl className="py-2 mb-3">
             <dt>누적 복망고 수</dt>
-            <dd className="text-2xl text-primary-normal">12,578</dd>
+            <dd className="text-2xl text-primary-normal">
+              {countUp && (
+                <CountUp
+                  start={0}
+                  end={12587}
+                  separator={","}
+                  duration={1.5}
+                  delay={0}
+                />
+              )}
+            </dd>
           </dl>
           <dl className="py-2">
             <dt>누적 덕담 메세지 수</dt>
-            <dd className="text-2xl text-secondary-normal">123,456,789</dd>
+            <dd className="text-2xl text-secondary-normal">
+              {countUp && (
+                <CountUp
+                  start={0}
+                  end={123456789}
+                  separator={","}
+                  duration={1.5}
+                  delay={1}
+                />
+              )}
+            </dd>
           </dl>
         </div>
         <div className="p-4 mobile:pr-10">

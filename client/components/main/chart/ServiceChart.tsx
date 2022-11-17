f10f9191064e6e2d4c84896f4937dffaac693e4c
MA <36831218+rmaomina@users.noreply.github.com>
2022-11-17 23:01:51 +0900

    feat/메인 차트 구현
    메인 차트 섹션에 차트를 추가

--- client/components/main/chart/ServiceChart.tsx
+++ client/components/main/chart/ServiceChart.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LineChart from "./Chart";
 
+const getCurrentTime = (): string => {
+  const koDtf = new Intl.DateTimeFormat("ko", {
+    dateStyle: "long",
+    timeStyle: "medium",
+  }).format(new Date());
+  return koDtf;
+};
+
 const ServiceChart = () => {
+  const [currentTime, setCurrentTime] = useState("");
+
+  useEffect(() => {
+    setCurrentTime(getCurrentTime());
+  }, []);
+
   return (
     <div>
-      <div className="grid grid-cols-1 text-center mobile:text-left mobile:grid-cols-2">
+      <div className="grid w-full grid-cols-1 text-center mobile:text-left mobile:grid-cols-2">
         <div className="flex flex-col p-4 pt-8 mobile:pl-[6rem]">
           <dl className="py-2 mb-3">
             <dt>누적 복망고 수</dt>
@@ -15,19 +29,14 @@ const ServiceChart = () => {
             <dd className="text-2xl text-secondary-normal">123,456,789</dd>
           </dl>
         </div>
-        <div className="p-4 mobile:pr-20">
-          <div className="h-full border-2 rounded-lg bg-primary-normal/80 border-primary-normal aspect-[4/3]">
-            {/* <LineChart /> */}
+        <div className="p-4 mobile:pr-10">
+          <div className="relative h-full border-2 rounded-l border-mono-borderLight aspect-[4/2]">
+            <LineChart />
           </div>
         </div>
       </div>
       <div className="mt-4 text-center">
-        <p className="font-semibold text-secondary-darker">
-          2022.11.11 22:00:00 기준
-        </p>
-        <p className="text-mono-textDisabled">
-          1시간 간격으로 업데이트되고 있습니다
-        </p>
+        <p className="text-mono-textDisabled">{currentTime} 기준</p>
       </div>
     </div>
   );

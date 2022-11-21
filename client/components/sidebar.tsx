5b41c22c39d21ccf1e4b73b5f3414fefaaafd25e
Mina Kim <rmaomina@gmail.com>
2022-11-21 17:36:27 +0900

    feat/사이드바 딤 클릭시 닫기 기능

--- client/components/sidebar.tsx
+++ client/components/sidebar.tsx
@@ -1,37 +1,48 @@
-import { useSelector } from "react-redux";
-import { selectSidebarState } from "../store/sidebarSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { selectSidebarState, setSidebarState } from "../store/sidebarSlice";
 import Link from "next/link";
 
 const Sidebar = () => {
+  const dispatch = useDispatch();
   const sidebarState = useSelector(selectSidebarState);
 
+  const hideSidebar = () => {
+    dispatch(setSidebarState(false));
+  };
+
   return (
-    <div
-      className={
-        sidebarState
-          ? "mg-sidebar mg-sidebar-show"
-          : "mg-sidebar mg-sidebar-hide"
-      }
-    >
-      <ul>
-        <li className="p-2 mb-1">
-          <Link
-            href="/mypage"
-            className="hover:font-medium hover:text-primary-normal"
-          >
-            로그인/로그아웃
-          </Link>
-        </li>
-        <li className="p-2 mb-1">
-          <Link
-            href="/mypage"
-            className="hover:font-medium hover:text-primary-normal"
-          >
-            마이페이지
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div
+        className={sidebarState ? "mg-sidebar-dim" : "hidden"}
+        onClick={hideSidebar}
+      ></div>
+      <div
+        className={
+          sidebarState
+            ? "mg-sidebar mg-sidebar-show"
+            : "mg-sidebar mg-sidebar-hide"
+        }
+      >
+        <ul>
+          <li className="p-2 mb-1">
+            <Link
+              href="/login"
+              className="hover:font-medium hover:text-primary-normal"
+            >
+              로그인
+            </Link>
+          </li>
+          <li className="p-2 mb-1">
+            <Link
+              href="/mypage"
+              className="hover:font-medium hover:text-primary-normal"
+            >
+              마이페이지
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 

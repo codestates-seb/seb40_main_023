39b9c54b691d7c296473056157af0208062b81d9
Mina Kim <rmaomina@gmail.com>
2022-11-23 19:52:16 +0900

    chore/header, sidebar 대소문자 수정

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,4 @@
-import Header from "../components/header";
-import Sidebar from "../components/sidebar";
+import Header from "../components/Header";
 import Countdown from "../components/main/info/Countdown";
 import Section from "../components/main/Section";
 import SectionTitle from "../components/main/SectionTitle";

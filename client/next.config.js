66e225984227404049b7c322c106d856394024ec
MA <36831218+rmaomina@users.noreply.github.com>
2022-11-23 09:26:04 +0900

    feat/리뷰 슬라이드 패치 초기 세팅

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,20 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
+const BASE_URL = process.env.NEXT_PUBLIC_SERVER_URL;
+
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+
+  async rewrites() {
+    console.log(BASE_URL);
+    return [
+      {
+        source: "/api/:slug*",
+        destination: `${BASE_URL}/:slug*`,
+      },
+    ];
+  },
 };
+
+module.exports = nextConfig;

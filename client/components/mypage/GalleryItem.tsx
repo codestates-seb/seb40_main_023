ee6a6a478aaeb9ceacbe48ed3809e1bb5050ad4c
Exist95 <jyeol0210@naver.com>
2022-11-29 14:04:42 +0900

    feat/복망고 Delete 로직 구현

--- client/components/mypage/GalleryItem.tsx
+++ client/components/mypage/GalleryItem.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 import Link from "next/link";
+import axios from "axios";
+import { getCookie } from "../util/cookie";
 
 const GalleryItem = ({ bgImage, userId, luckMangoId, title }: any) => {
+  const DeleteLuckMango = async () => {
+    try {
+      await axios({
+        method: "DELETE",
+        url: `/api/luckMango/${luckMangoId}`,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getCookie("accessJwtToken")}`,
+        },
+      });
+      location.reload();
+    } catch (error) {}
+  };
+
   return (
     <div className={`group gap-2 mg-default-card`}>
       <div className="mg-card-contents">
@@ -14,34 +30,29 @@ const GalleryItem = ({ bgImage, userId, luckMangoId, title }: any) => {
           <div className="truncate">
             <span className="font-medium">{title}</span>님의 새해 복망고
           </div>
-          <div className="truncate">개의 덕담을 받았어요!</div>
+          <div className="truncate">{luckMangoId}개의 덕담을 받았어요!</div>
         </div>
       </div>
       <div className={`mg-card-overlay`}>
         {/* 수정페이지 */}
-
-        <div className="mg-card-button bg-[url(/images/ico/ico-card-edit.svg)]">
-          <Link href={`/edit/${luckMangoId}`}></Link>
-        </div>
-
+        <Link href={`/edit/${luckMangoId}`}>
+          <div className="mg-card-button bg-[url(/images/ico/ico-card-edit.svg)]"></div>
+        </Link>
         {/* 삭제버튼 */}
 
-        <div className="mg-card-button bg-[url(/images/ico/ico-card-delete.svg)]">
-          <Link href={`/edit/${luckMangoId}`}> </Link>
-        </div>
+        <div
+          className="mg-card-button bg-[url(/images/ico/ico-card-delete.svg)]"
+          onClick={() => DeleteLuckMango()}
+        ></div>
 
         {/* qr코드 */}
-
-        <div className="mg-card-button bg-[url(/images/ico/ico-card-qr.svg)]">
-          <Link href={`/edit/${luckMangoId}`}> </Link>
-        </div>
-
+        <Link href={`/edit/${luckMangoId}`}>
+          <div className="mg-card-button bg-[url(/images/ico/ico-card-qr.svg)]"></div>
+        </Link>
         {/* link 복사 */}
-
-        <div className="mg-card-button bg-[url(/images/ico/ico-card-url.svg)]">
-          {" "}
-          <Link href={`/edit/${luckMangoId}`}></Link>
-        </div>
+        <Link href={`/edit/${luckMangoId}`}>
+          <div className="mg-card-button bg-[url(/images/ico/ico-card-url.svg)]"></div>
+        </Link>
       </div>
     </div>
   );

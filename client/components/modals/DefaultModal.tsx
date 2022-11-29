ea5a4e28458deacae64b8645f039e444576b6c9a
Exist95 <jyeol0210@naver.com>
2022-11-29 16:37:18 +0900

    feat/모달창 수정

--- client/components/modals/DefaultModal.tsx
+++ client/components/modals/DefaultModal.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import closed from "../../public/images/ico/ico-modal-close.svg";
-
+//마이페이지용 모달로 사용하겠습니다.
 const DefaultModal = ({
   title,
   contents,
@@ -11,31 +11,35 @@ const DefaultModal = ({
   setModal,
 }: any) => {
   return (
-    <div className="absolute top-0 bottom-0 left-0 right-0 bg-mono-400 z-999">
-      <div className="absolute top-[50%] left-[50%] bg-white border rounded-xl -translate-x-2/4 -translate-y-2/4 p-2 box-border">
+    <div className="fixed top-0 bottom-0 left-0 right-0 z-50 bg-mono-400">
+      <div className="absolute top-[50%] left-[50%] bg-white border rounded-xl -translate-x-2/4 -translate-y-2/4 p-2 box-border z-999 w-[300px] h-[200px]">
         <header className="flex justify-end w-full hover:cursor-pointer">
-          <Image src={closed} alt="closed" onClick={() => setModal(false)} />
+          <Image
+            src={closed}
+            alt="closed button"
+            onClick={() => setModal(false)}
+          />
         </header>
-        <section className="">
-          <main className="flex flex-col justify-center gap-1 text-center">
+        <div>
+          <main className="flex flex-col gap-1 text-center">
             <div>{title}</div>
             <div className="whitespace-pre-line text-mono-500">{contents}</div>
             <div className=" text-mono-400">{confirm}</div>
+            <footer className="flex justify-center gap-5 p-3">
+              {Nobutton && (
+                <button
+                  className="mg-negative-button-round"
+                  onClick={() => setModal(false)}
+                >
+                  {Nobutton}
+                </button>
+              )}
+              <button className="rounded-full mg-primary-button">
+                {Yesbutton}
+              </button>
+            </footer>
           </main>
-        </section>
-        <footer className="flex justify-center gap-5 p-2 mt-3">
-          {Nobutton && (
-            <button
-              className="mg-negative-button-round"
-              onClick={() => setModal(false)}
-            >
-              {Nobutton}
-            </button>
-          )}
-          <button className="rounded-full mg-primary-button">
-            {Yesbutton}
-          </button>
-        </footer>
+        </div>
       </div>
     </div>
   );

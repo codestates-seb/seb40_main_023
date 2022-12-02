69ab005cf412050e4813548b0065dc7dc1e0f9b2
saypart <saypart@gmail.com>
2022-12-02 13:58:42 +0900

    s3 업로드 버그 수정

--- server/src/main/java/seb40/main023/server/awsS3/service/S3UpFileService.java
+++ server/src/main/java/seb40/main023/server/awsS3/service/S3UpFileService.java
@@ -39,13 +39,13 @@ public class S3UpFileService {
 
         S3UpFile s3UpFiles = new S3UpFile();
         s3UpFiles.setUpFileName(s3FileName);
-        s3UpFiles.setUpFileUrl(amazonS3.getUrl(bucket, s3FileName).toString());
+        s3UpFiles.setUpFileUrl("https://saypart.s3.ap-northeast-2.amazonaws.com/login/"+s3FileName);
         s3UpFiles.setMember(memberService.findVerifiedMember(s3UpFile.getMember().getMemberId())); // 멤버 세팅
-        s3UpFiles.getMember().setImgUrl(amazonS3.getUrl(bucket, s3FileName).toString());
+        s3UpFiles.getMember().setImgUrl(s3UpFiles.getUpFileUrl());
         s3UpFileRepository.save(s3UpFiles);;
 
 
-        return amazonS3.getUrl(bucket, s3FileName).toString();
+        return "프로필 이미지 업로드 완료";
     }
 
     public String upload2(MultipartFile multipartFile, S3UpFile s3UpFile) throws IOException {
@@ -59,12 +59,12 @@ public class S3UpFileService {
 
         S3UpFile s3UpFiles = new S3UpFile();
         s3UpFiles.setUpFileName(s3FileName);
-        s3UpFiles.setUpFileUrl(amazonS3.getUrl(bucket, s3FileName).toString());
+        s3UpFiles.setUpFileUrl("https://saypart.s3.ap-northeast-2.amazonaws.com/luckMango/"+s3FileName);
         s3UpFiles.setLuckMango(luckMangoService.findVerifiedLuckMango(s3UpFile.getLuckMango().getLuckMangoId())); // 럭망고 세팅
         s3UpFiles.getLuckMango().setBgImage(amazonS3.getUrl(bucket, s3FileName).toString());
         s3UpFileRepository.save(s3UpFiles);;
 
 
-        return amazonS3.getUrl(bucket, s3FileName).toString();
+        return "복망고 배경 이미지 업로드 완료";
     }
 }

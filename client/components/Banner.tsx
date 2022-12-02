d45713ab538195a8633872f95ef898faaf61d615
Mina Kim <rmaomina@gmail.com>
2022-12-02 13:18:10 +0900

    fix/메인 스타일 수정 및 복망고 프리뷰 수정

--- client/components/Banner.tsx
+++ client/components/Banner.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const Banner = () => {
   return (
-    <Link href="/" className="mg-width-size">
-      <div className="h-[71.98px] relative rounded-[10px] mg-primary-button mg-width-size mg-flex-center justify-end cursor-pointer">
-        <div className="bg-[url(/images/char/char-banner.svg)] w-[117px] h-[130px] absolute top-[-50px] left-2" />
-        <div className="mg-flex-center justify-center mg-width-size h-[71.98px]">
-          <div className="ml-16 mr-9">
-            나도 새해 복망고 만들어볼까?
-            <div className="font-semibold">새해 복망고 메인으로 이동</div>
+    <Link href="/" className="block py-4">
+      <div className="relative rounded-md mg-primary-button py-3 pl-[90px] pr-[20px] after:content-['〉'] after:absolute after:right-[12px] after:top-[50%] after:translate-y-[-50%] after:text-white after:font-bold after:text-xl">
+        <Image
+          width={117}
+          height={130}
+          src="/images/char/char-banner.svg"
+          alt="유혹의 복망고 캐릭터"
+          className="absolute top-[-50px] left-[-10px]"
+        />
+        <div className="justify-center mg-flex-center">
+          <div className="">
+            <p>나도 새해 복망고 만들어볼까?</p>
+            <p className="font-medium">새해 복망고 메인으로 이동</p>
           </div>
-          <div className="text-xl font-bold cursor-pointer">〉</div>
+          <div className="text-xl font-bold cursor-pointer"></div>
         </div>
       </div>
     </Link>

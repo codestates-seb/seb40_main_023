6eab50ed1c3ccfb6f2cf89e465388fc8860a0b6b
hongdahee <heewhy3513@gmail.com>
2022-12-04 13:15:15 +0900

    fix/session Storage에 저장

--- client/recoil/luckMgId.ts
+++ client/recoil/luckMgId.ts
@@ -1,7 +1,13 @@
 import { atom } from "recoil";
 import { recoilPersist } from "recoil-persist";
 
-const { persistAtom } = recoilPersist();
+const sessionStorage =
+  typeof window !== "undefined" ? window.sessionStorage : undefined;
+
+const { persistAtom } = recoilPersist({
+  key: "luckMgIdState",
+  storage: sessionStorage,
+});
 
 export const luckMgIdState = atom<number>({
   key: `luckMgIdState`,

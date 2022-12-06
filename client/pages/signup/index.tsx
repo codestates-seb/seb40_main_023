4241dabb0a9a89539b8a42bdafdae7dcbe676d6c
Exist95 <96723716+Exist95@users.noreply.github.com>
2022-12-06 17:34:53 +0900

    fix/이메일 중복 검사 기능 추가
    * fix/이메일 중복 검사 기능 추가
    * chore/react-icon 제거

--- client/pages/signup/index.tsx
+++ client/pages/signup/index.tsx
@@ -25,26 +25,23 @@ const Signup = () => {
   const router = useRouter();
 
   //폼 만들기
-  const signupSubmit = (e: any) => {
+  const signupSubmit = async (e: any) => {
     e.preventDefault();
-    window.setTimeout("window.location.reload()", 2000);
-    notifyError({
-      message: "이미 존재하는 닉네임이거나 이메일입니다.",
-      icon: "😎",
+    await axios({
+      method: "post",
+      url: `/api/member`,
+      data: {
+        name: id,
+        email: email,
+        password: password,
+      },
+    }).catch(function (error) {
+      if (error.response.data.message === "Member exists") {
+        notifyError({ message: "이메일이 중복되었습니다.", icon: "🥭" });
+        setEmail("");
+        setIsEmail(false);
+      }
     });
-    try {
-      axios
-        .post("api/member", {
-          name: id,
-          email: email,
-          password: password,
-        })
-        .then(res => {
-          router.push("/login");
-        });
-    } catch (error) {
-      console.warn(error);
-    }
   };
 
   //아이디
@@ -169,6 +166,7 @@ const Signup = () => {
                     <input
                       id="email"
                       type="text"
+                      value={email}
                       placeholder="이메일 형식에 맞게 입력해주세요"
                       onChange={onChangeEmail}
                       className={`mg-default-input w-full ${

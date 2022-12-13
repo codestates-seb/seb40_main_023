3597b93244862bf45c2d773daeb249d0a618a697
Mina Kim <rmaomina@gmail.com>
2022-12-13 21:25:56 +0900

    feat/알림 모달  디폴트로 false 처리

--- client/components/AlertMessage.tsx
+++ client/components/AlertMessage.tsx
@@ -4,15 +4,20 @@ import Link from "next/link";
 const AlertMessage = () => {
   return (
     <ul className="px-5 py-4 rounded-xl bg-white shadow-context w-[calc(100%-40px)] max-h-[400px] overflow-y-auto min-h-[40px] absolute top-[calc(100%+14px)] left-[20px]">
-      <li className="my-1 text-base text-mono-600">
-        <Link
-          href="www.naver.com"
-          className="underline underline-offset-4 decoration-mono-100"
-        >
-          <span className="text-primary-normal">00</span>님께서 보내주신 덕담이
-          있습니다.
-        </Link>
-      </li>
+      {/* {data.map(el => {
+        return (
+          <li className="my-1 text-base text-mono-600">
+            <Link
+              href="www.naver.com"
+              className="underline underline-offset-4 decoration-mono-100"
+            >
+              <span className="text-primary-normal">00</span>의 복망고에{" "}
+              <span className="text-primary-normal">N</span>개의 덕담이
+              도착했습니다!
+            </Link>
+          </li>
+        );
+      })} */}
     </ul>
   );
 };
--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 import Image from "next/image";
 import Sidebar from "./Sidebar";
 import AlertMessage from "./AlertMessage";
+import { userState } from "../recoil/user";
 import { useCookies } from "react-cookie";
 import { useRecoilState } from "recoil";
-import { userState } from "../recoil/user";
 import { memberIdState } from "../recoil/memberId";
 
 const Header = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [isAlertOpen, setIsAlertOpen] = useState(true);
+  const [isAlertOpen, setIsAlertOpen] = useState(false);
   const [cookies] = useCookies(["accessJwtToken"]);
   const [user, setUser] = useRecoilState(userState);
   const [memberId, setMemberId] = useRecoilState(memberIdState);
@@ -34,26 +34,33 @@ const Header = () => {
 
   useEffect(() => {
     tokenValid();
+    console.log(user);
+    console.log(user.login);
+    console.log(memberId);
   }, [isSidebarOpen]);
+
+  useEffect(() => {}, []);
 
   return (
     <>
       <header className="border-b border-mono-borderLight bg-white z-[999] fixed h-[58px] px-3 shadow-header flex items-center w-full justify-between">
         <div className="flex justify-center items-center basis-[40px] shrink-0 grow-0">
-          <button className="relative flex items-center justify-center">
-            <Image
-              src="/images/ico/ico-alert.svg"
-              width={19}
-              height={20}
-              alt="받은 덕담 알림"
-              onClick={toggleAlertHandle}
-            />
-            <span className="absolute top-0 right-0 z-10 flex w-2 h-2">
-              <span className="absolute inline-flex w-full h-full rounded-full opacity-30 animate-ping bg-danger-normal"></span>
-              <span className="relative inline-flex w-2 h-2 rounded-full bg-danger-normal"></span>
-            </span>
-          </button>
-          {isAlertOpen && <AlertMessage />}
+          {
+            <button className="relative flex items-center justify-center">
+              <Image
+                src="/images/ico/ico-alert.svg"
+                width={19}
+                height={20}
+                alt="받은 덕담 알림"
+                onClick={toggleAlertHandle}
+              />
+              <span className="absolute top-0 right-0 z-10 flex w-2 h-2">
+                <span className="absolute inline-flex w-full h-full rounded-full opacity-30 animate-ping bg-danger-normal"></span>
+                <span className="relative inline-flex w-2 h-2 rounded-full bg-danger-normal"></span>
+              </span>
+            </button>
+          }
+          {isAlertOpen && !isSidebarOpen && <AlertMessage />}
         </div>
         <div className="flex justify-center w-full">
           <h1 className="mg-logo">
@@ -86,8 +93,8 @@ const Header = () => {
         </button>
       </header>
       <Sidebar
-        toggleHandler={toggleSidebarHandle}
         toggleState={isSidebarOpen}
+        toggleHandler={toggleSidebarHandle}
         setIsSidebarOpen={setIsSidebarOpen}
       />
     </>
